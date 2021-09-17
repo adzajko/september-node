0413739d4691a69ba374d101b126686d10b39a42
adzajko <adzajkoski@gmail.com>
2021-09-17 22:38:16 +0200

    Added class 08 materials

--- class-08/movie-db-api/pkg/User/index.js
+++ class-08/movie-db-api/pkg/User/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-const User = mongoose.model('users');
+const User = mongoose.model('users', {
+  username: String,
+  password: String
+});
 
 module.exports = User;
--- class-08/movie-db-api/server.js
+++ class-08/movie-db-api/server.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
+const { connectToDb } = require('./db');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
 
+connectToDb()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log('Server running');
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
