ce9250769b8a876ca303e3cb29216e09c0f54760
adzajko <adzajkoski@gmail.com>
2021-09-17 22:44:16 +0200

    Comments on nodemailer

--- class-08/movie-db-api/pkg/mailer/index.js
+++ class-08/movie-db-api/pkg/mailer/index.js
@@ -1,6 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const sendMailModel = async mailBody => {
+  /**
+   * Node Mailer requires a transport to work.
+   * A transport is a definition of a mail service, can be outlook / gmail ...
+   * the auth part is the credentials of YOUR email.
+   * Some email services will ask for more authentication. (Gmail especially.)
+   */
   const transport = nodemailer.createTransport({
     service: 'outlook',
     auth: {
@@ -9,6 +15,11 @@ const sendMailModel = async mailBody => {
     }
   });
 
+  /**
+   * The mail options object contains at least 4 options.
+   * the from and to options should be your user. (Fastest workaround for credentials)
+   * subject and text are up to you :)
+   */
   const mailOptions = {
     from: process.env.MAIL_USER,
     to: process.env.MAIL_USER,
@@ -16,6 +27,9 @@ const sendMailModel = async mailBody => {
     text: `${mailBody.name}: \n ${mailBody.message}`
   };
 
+  /**
+   * Once the mailer configuration is complete, just use the .sendMail function with the above options as an argument.
+   */
   await transport.sendMail(mailOptions);
 };
 
